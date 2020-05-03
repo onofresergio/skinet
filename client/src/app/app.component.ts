0650b3ed47a49ad23df9f5e403f6a253a5c64ddd
onofresergio <onofresergio@yahoo.com.br>
2020-05-03 20:07:05 -0300

    section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/products';
 import { Component, OnInit } from '@angular/core';
@@ -12,9 +13,16 @@ export class AppComponent implements OnInit {
 
   title = 'Skinet';
 
-    constructor(){}
+    constructor(private basketService: BasketService){}
 
   ngOnInit() {
-
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('Initialized basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './../../basket/basket.service';
 import { ShopService } from './../shop.service';
 import { IProduct } from './../../shared/models/products';
 import { Component, OnInit } from '@angular/core';
@@ -11,15 +12,31 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(private shopService: ShopService,
               private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService) {
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
                 this.bcService.set('@productDetails', '');
               }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 
   loadProduct() {
